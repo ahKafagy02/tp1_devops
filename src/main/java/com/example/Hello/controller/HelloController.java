13d5fe10701df6e52a9a0ab4c5be24e7e3577d2b
Ahmed Kafagy <ahkafagy02@gmail.com>
2026-04-09 10:16:32 +0200

    add Car entity, CarService and REST endpoints for car management

--- src/main/java/com/example/Hello/controller/HelloController.java
+++ src/main/java/com/example/Hello/controller/HelloController.java
@@ -1,13 +1,34 @@
 package com.example.Hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Hello.entities.Car;
+import com.example.Hello.services.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private CarService carService;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @PostMapping("/cars")
+    public void addCar(@RequestBody Car car) {
+        carService.addCar(car);
+    }
+
+    @GetMapping("/cars")
+    public List<Car> getCars() {
+        return carService.getCars();
+    }
+
+    @GetMapping("/cars/{plateNumber}")
+    public Car getCar(@PathVariable String plateNumber) {
+        return carService.getCar(plateNumber);
     }
 }
